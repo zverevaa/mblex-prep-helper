914c05822a288933e451777b15c95211095dd751
Kira Zvereva <angryvex@gmail.com>
2024-10-24 10:54:43 +0300

    Add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
+import Header from "@/components/header";
 import Questions from "@/components/questions";
 
 export default function Home() {
-    return (
-        <div className="p-6">
-            <Questions />
-        </div>
-    );
+    return <Questions />;
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="flex justify-between items-center h-14 p-3 bg-purple-300 rounded-b-md">
+            <Link href="/">
+                <h1 className="text-2xl font-bold">MBLEX PREP</h1>
+            </Link>
+            <Link href="/add">Storage</Link>
+        </header>
+    );
+}
